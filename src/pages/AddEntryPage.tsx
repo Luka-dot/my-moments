e5fc47b8d57ec4167fc6c1c79dfc0b5d8b406522
Luka-dot <79lukask@gmail.com>
2021-05-10 20:20:32 -0400

    form added to add

--- src/pages/AddEntryPage.tsx
+++ src/pages/AddEntryPage.tsx
@@ -1,15 +1,28 @@
 import {
   IonBackButton,
+  IonButton,
   IonButtons,
   IonContent,
   IonHeader,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonList,
   IonPage,
+  IonTextarea,
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
 import React, { useState, useEffect } from "react";
 
 const AddEntryPage: React.FC = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleSave = () => {
+    console.log("save prop: ", { title, description });
+  };
+
   return (
     <IonPage>
       <IonHeader>
@@ -20,7 +33,27 @@ const AddEntryPage: React.FC = () => {
           <IonTitle>ADD Entry</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent className="ion-padding">TODO</IonContent>
+      <IonContent className="ion-padding">
+        <IonList>
+          <IonItem>
+            <IonLabel position="stacked">Title</IonLabel>
+            <IonInput
+              value={title}
+              onIonChange={(event) => setTitle(event.detail.value)}
+            />
+          </IonItem>
+          <IonItem>
+            <IonLabel position="stacked">Description</IonLabel>
+            <IonTextarea
+              value={description}
+              onIonChange={(event) => setDescription(event.detail.value)}
+            />
+          </IonItem>
+          <IonButton expand="block" onClick={handleSave}>
+            Save
+          </IonButton>
+        </IonList>
+      </IonContent>
     </IonPage>
   );
 };
