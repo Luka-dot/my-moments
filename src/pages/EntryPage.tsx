70b30b3f4a1058ec042364eb3bec3ee8348bf393
Luka-dot <79lukask@gmail.com>
2021-05-10 20:50:53 -0400

    delete entries functionality added

--- src/pages/EntryPage.tsx
+++ src/pages/EntryPage.tsx
@@ -1,17 +1,21 @@
 import {
   IonBackButton,
+  IonButton,
   IonButtons,
   IonContent,
   IonHeader,
+  IonIcon,
   IonPage,
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router";
+import { useHistory, useParams } from "react-router";
 import { firestore } from "../firebase";
 import { Entry, toEntry } from "../Models";
 import { useAuth } from "../Auth";
+
+import { trash as trashIcon } from "ionicons/icons";
 
 interface RouterParams {
   id: string;
@@ -19,6 +23,7 @@ interface RouterParams {
 
 const EntryPage: React.FC = () => {
   const { userId } = useAuth() as any;
+  const history = useHistory();
   const { id } = useParams<RouterParams>();
   const [entry, setEntry] = useState<Entry>();
 
@@ -33,12 +38,27 @@ const EntryPage: React.FC = () => {
     });
   }, [id, userId]);
 
+  const handleDelete = async () => {
+    const entryRef = firestore
+      .collection("users")
+      .doc(userId)
+      .collection("entries")
+      .doc(id);
+    await entryRef.delete();
+    history.goBack();
+  };
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
           <IonButtons slot="start">
             <IonBackButton />
+          </IonButtons>
+          <IonButtons slot="end">
+            <IonButton onClick={handleDelete}>
+              <IonIcon icon={trashIcon} slot="icon-only" />
+            </IonButton>
           </IonButtons>
           <IonTitle>Entry for {entry?.title}</IonTitle>
         </IonToolbar>
