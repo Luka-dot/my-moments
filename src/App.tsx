52d27206cd55aeb918a89d45e8ac52ebce2f409c
Luka-dot <79lukask@gmail.com>
2022-01-17 16:16:06 -0500

    added OneSignal push notifications

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React, { } from "react";
 import { IonReactRouter } from "@ionic/react-router";
 import { Redirect, Route, Switch } from "react-router-dom";
 import { connect } from 'react-redux';
+import OneSignal from 'onesignal-cordova-plugin';
 
 import LoginPage from "./pages/LoginPage";
 import AppTabs from "./AppTabs";
@@ -10,7 +11,26 @@ import NotFoundPage from "./pages/NotFoundPage";
 import RegisterPage from "./pages/RegisterPage";
 import TeamSelectionPage from "./pages/TeamSelectionPage";
 
+// Call this function when your app starts
+function OneSignalInit(): void {
+  // Uncomment to set OneSignal device logging to VERBOSE  
+  // OneSignal.setLogLevel(6, 0);
+
+  // NOTE: Update the setAppId value below with your OneSignal AppId.
+  OneSignal.setAppId("fb954bfe-7d60-443d-a7dd-695ffd616880");
+  OneSignal.setNotificationOpenedHandler(function (jsonData) {
+    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+  });
+
+  // iOS - Prompts the user for notification permissions.
+  //    * Since this shows a generic native prompt, we recommend instead using an In-App Message to prompt for notification permission (See step 6) to better communicate to your users what notifications they will get.
+  OneSignal.promptForPushNotificationsWithUserResponse(function (accepted) {
+    console.log("User accepted notifications: " + accepted);
+  });
+}
+
 const App: React.FC = (props: any) => {
+  OneSignalInit()
   // const { loading, auth } = useAuthInit();
   //const { data } = useAuthInit2(auth);
   const { loggedIn } = props.currentUser;
