b50182bf45b81f21ec20d46cfa118d840691ee81
Luka-dot <79lukask@gmail.com>
2022-02-21 10:29:30 -0500

    chat page changes and tabs

--- src/pages/TeamChatPage.tsx
+++ src/pages/TeamChatPage.tsx
@@ -1,34 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { connect } from 'react-redux'
-import { IonCol, IonFooter, IonHeader, IonItem, IonList, IonPage, IonRow, IonText } from '@ionic/react';
+import { IonAvatar, IonCol, IonFooter, IonHeader, IonItem, IonList, IonPage, IonRow, IonText, IonTitle, IonToolbar } from '@ionic/react';
 
 import './teamChatPage.css';
 import ChatItem from '../components/chat/ChatItem';
 
 export const TeamChatPage = (props: any) => {
-    return (
-        <IonPage>
-            {/* <IonRow>
-                <IonCol>
-                    <IonHeader>
-                        <IonText>Welcome to Team Chat</IonText>
-                    </IonHeader>
-                </IonCol>
-            </IonRow> */}
-            <IonList>
-                {
-                    props.team.members.map((member) => <ChatItem contact={member} />)
-                }
-            </IonList>
-            <IonFooter >
-                <IonItem >chat text field</IonItem>
-            </IonFooter>
-        </IonPage>
-    )
+    const [selectedTab, setSelectedTab] = useState(true)
+    console.log(selectedTab)
+
+    function toggleTabs() {
+        console.log('setting')
+        setSelectedTab(!selectedTab)
+    }
+
+    if (selectedTab === true) {
+        return (
+            <IonPage>
+                <IonHeader>
+                    <IonToolbar>
+                        <IonRow >
+                            <IonItem lines="none" >
+                                <IonTitle  >Chat</IonTitle>
+                            </IonItem>
+                            <IonItem lines="none" onClick={toggleTabs}>
+                                <IonText>Contacts</IonText>
+                            </IonItem>
+                        </IonRow>
+                    </IonToolbar>
+                </IonHeader>
+                <IonList>
+                    {
+                        props.team.members.map((member) => <ChatItem contact={member} />)
+                    }
+                </IonList>
+                <IonFooter >
+                    <IonItem >chat text field</IonItem>
+                </IonFooter>
+            </IonPage>
+        )
+    } else if (selectedTab === false) {
+        return (
+            <IonPage>
+                <IonHeader>
+                    <IonToolbar>
+                        <IonRow >
+                            <IonItem lines="none" onClick={toggleTabs}>
+                                <IonText>Chat</IonText>
+                            </IonItem>
+                            <IonItem lines="none">
+                                <IonTitle >Contacts</IonTitle>
+                            </IonItem>
+                        </IonRow>
+                    </IonToolbar>
+                </IonHeader>
+                <IonList>
+                    {
+                        props.team.members.map((member) => <ChatItem contact={member} />)
+                    }
+                </IonList>
+            </IonPage>
+        )
+    }
 }
 
 const mapStateToProps = (state) => ({
     team: state.team,
+    currentUser: state.auth.curentUserDetails,
 })
 
 export default connect(mapStateToProps, {})(TeamChatPage)
@@ -59,3 +97,8 @@ export default connect(mapStateToProps, {})(TeamChatPage)
 //                     </IonCol>
 //                 </IonList>
 //             </IonContent>
+
+
+// <IonAvatar>
+//                         <img src={props.currentUser.pictureUrl} alt="" />
+//                     </IonAvatar>
